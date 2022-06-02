373a241478b6ea0b9e56c95e7969d6bf7ad46f92
kevinzhow <kevinchou.c@gmail.com>
2022-06-02 11:18:07 +0800

    update(wasm3)

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .package(
             name: "CWasm3",
             url: "https://github.com/kevinzhow/cwasm3.git",
-            from: "0.5.4"),
+            from: "0.5.5"),
         .package(
             name: "Synchronized",
             url: "https://github.com/shareup/synchronized.git",
